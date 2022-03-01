daca812d71885d7df8ae22850bf46d7cf462d771
Adam <adam.crespo101@outlook.com>
2022-03-01 09:25:31 -0500

    commit tree 700a228c85593aee4b27ba988e634a689ed71676

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => { 
+    update();
+})
+
+function update () {
+    document.getElementById('text').textContent = 'This is really cool!'
+}
+
+//Why is there an error when using .innerText rather than .innerHTML
